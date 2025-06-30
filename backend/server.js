555a8dc4caac8461e945d55873f9e6a6c6e8ae72
Arunkumar552004 <arunk820406@gmail.com>
2025-06-30 16:07:02 +0530

    fix: set server to listen on process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
 
